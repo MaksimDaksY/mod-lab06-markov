e3da3d09cca33415b91e38f8dcb4f658f5cfdcb9
Maksim Drobyak <maksim_d_2016@mail.ru>
2026-05-08 14:57:05 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,24 +1,24 @@
 // Copyright 2026 Maksim Drobyak
 
-#include "textgen.h"
 #include <iostream>
 #include <string>
 
+#include "textgen.h"
+
 int main(int argc, char* argv[]) {
+  int npref = 2;
+  int maxgen = 1000;
+  std::string inputFile = "input.txt";
+  std::string outputFile = "result/gen.txt";
 
-    int npref = 2;
-    int maxgen = 1500;
-    std::string inputFile = "input.txt";
-    std::string outputFile = "result/gen.txt";
+  if (argc > 1) inputFile = argv[1];
+  if (argc > 2) maxgen = std::stoi(argv[2]);
+  if (argc > 3) npref = std::stoi(argv[3]);
 
-    if (argc > 1) inputFile = argv[1];
-    if (argc > 2) maxgen = std::stoi(argv[2]);
-    if (argc > 3) npref = std::stoi(argv[3]);
+  TextGenerator tg(npref);
+  tg.parseFile(inputFile);
+  tg.generate(maxgen, outputFile);
 
-    TextGenerator tg(npref);
-    tg.parseFile(inputFile);
-    tg.generate(maxgen, outputFile);
-
-    std::cout << "Generated text saved to " << outputFile << std::endl;
-    return 0;
+  std::cout << "Generated text saved to " << outputFile << std::endl;
+  return 0;
 }
